b7a2da95a7507226f51e54c2e9b8ee92e88f47e2
Nopey <hashtagsmrt@outlook.de>
2024-09-06 19:02:22 +1000

    update

--- src/components/content/player_leaderboards/PlayerLeaderboards.tsx
+++ src/components/content/player_leaderboards/PlayerLeaderboards.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import LeaderboardEntry from "./leaderboard_entry/LeaderboardEntry";
-import { UserData } from "../../../utils/Types";
+import userData from "../../../assets/fhex_data/user_data.json";
 
 interface Props {
   update: (userName: string) => void;
@@ -12,40 +12,20 @@ const PlayerLeaderboards = ({ update }: Props) => {
     setInputText(event.target.value.toLowerCase());
   };
 
-  const [entries, setEntries] = useState<JSX.Element[]>([]);
-  if (entries.length == 0 && inputText === "") {
-    for (let i = 0; i < 50; i++) {
-      entries.push(
-        <span className="placeholder rounded w-100 mb-1" key={"player" + i}>
-          -
-        </span>
-      );
+  const entries = [];
+  const sortedData = Object.values(userData).sort((a, b) => b.pp - a.pp);
+  for (let i = 0; i < 1000; i++) {
+    if (sortedData[i].username.toLowerCase().includes(inputText)) {
+      entries.push(<LeaderboardEntry key={sortedData[i].uuid} position={i + 1} username={sortedData[i].username} pp={sortedData[i].pp} onClick={() => update(sortedData[i].username)} />);
     }
   }
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const ent: JSX.Element[] = [];
-    fetch("public/fhex_data/user_data.json")
-      .then((res) => res.json())
-      .then((data: UserData[]) => {
-        const sortedData = Object.values(data).sort((a, b) => b.pp - a.pp);
-        for (let i = 0; i < 1000; i++) {
-          if (sortedData[i].username.toLowerCase().includes(inputText)) {
-            ent.push(<LeaderboardEntry key={sortedData[i].uuid} position={i + 1} username={sortedData[i].username} pp={sortedData[i].pp} onClick={() => update(sortedData[i].username)} />);
-          }
-        }
-        setEntries(ent);
-      });
-    return () => controller.abort();
-  }, [inputText, update]);
 
   return (
     <>
       <div className="container p-2 pb-3 mt-1">
         <h3 className="text-center">Player Leaderboards</h3>
         <input className="mx-auto text-center w-50 h-50 form-control mb-2" type="search" placeholder="Search User" onChange={handleSearchInput} />
-        <div className="placeholder-glow">{entries}</div>
+        {entries.length === 0 ? <h5 className="text-center pt-2">No player found.</h5> : entries}
       </div>
       <p className="text-center my-2">Made by Nopey</p>
     </>
--- src/components/content/track_list/TrackList.tsx
+++ src/components/content/track_list/TrackList.tsx
@@ -1,6 +1,6 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import TrackListEntry from "./track_list_entry/TrackListEntry";
-import { TrackData } from "../../../utils/Types";
+import trackData from "../../../assets/fhex_data/track_data.json";
 
 interface Props {
   update: (trackName: string) => void;
@@ -12,40 +12,20 @@ const TrackList = ({ update }: Props) => {
     setInputText(event.target.value.toLowerCase());
   };
 
-  const [entries, setEntries] = useState<JSX.Element[]>([]);
-  if (entries.length == 0 && inputText === "") {
-    for (let i = 0; i < 50; i++) {
-      entries.push(
-        <span className="placeholder rounded w-100 mb-1" key={"track" + i}>
-          -
-        </span>
-      );
+  const entries = [];
+  const sortedData = Object.values(trackData).sort((a, b) => b.records.length - a.records.length);
+  for (const track of sortedData) {
+    if (track.display_name.toLowerCase().includes(inputText)) {
+      entries.push(<TrackListEntry key={track.command_name} date={track.date_created} trackname={track.display_name} records={track.records.length} onClick={() => update(track.command_name)}></TrackListEntry>);
     }
   }
-
-  useEffect(() => {
-    const controller = new AbortController();
-    const ent: JSX.Element[] = [];
-    fetch("public/fhex_data/track_data.json")
-      .then((res) => res.json())
-      .then((data: TrackData[]) => {
-        const sortedData = Object.values(data).sort((a, b) => b.records.length - a.records.length);
-        for (const track of sortedData) {
-          if (track.display_name.toLowerCase().includes(inputText)) {
-            ent.push(<TrackListEntry key={track.command_name} date={track.date_created} trackname={track.display_name} records={track.records.length} onClick={() => update(track.command_name)}></TrackListEntry>);
-          }
-        }
-        setEntries(ent);
-      });
-    return () => controller.abort();
-  }, [inputText, update]);
 
   return (
     <>
       <div className="container p-2 pb-3 mt-1">
         <h3 className="text-center">Track List</h3>
         <input className="mx-auto text-center w-50 h-50 form-control mb-2" type="search" placeholder="Search Track" onChange={handleSearchInput} />
-        <div className="placeholder-glow">{entries}</div>
+        {entries.length === 0 ? <h5 className="text-center pt-2">No track found.</h5> : entries}
       </div>
       <p className="text-center my-2">Made by Nopey</p>
     </>
