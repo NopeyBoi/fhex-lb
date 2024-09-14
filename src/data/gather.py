3bc1e791002f0aef2a214733f1263f41ddffd758
Nopey <hashtagsmrt@outlook.de>
2024-09-14 03:48:17 +0200

    Add files via upload

--- src/data/gather.py
+++ src/data/gather.py
@@ -1,72 +1,116 @@
-import requests
+"""
+Data gathering script for fhex.nopey.online.
+Made by.. Nopey. (duh)
+"""
+
 import json
+import requests
+from colorama import Fore
 
-api_key = "?api_key=85c5e272-e9bf-4c81-91b5-ae6db61407a0"
+API_KEY = "?api_key=85c5e272-e9bf-4c81-91b5-ae6db61407a0"
 
-user_data = {}
-track_data = {}
+user_overview_data = {}
+user_specific_data = {}
+track_overview_data = {}
+track_specific_data = {}
 
 def get_api_path(track = ""):
-  version = 3
-  track_string = ""
-  if track != "":
-    version = 2;
-    track_string = f"/{track}/withusernames"
-  return f"http://frosthex.com:30050/api/v{version}/readonly/tracks{track_string}{api_key}"
+    """Create the API path based on the specified track"""
+    version = 3
+    track_string = ""
+    if track != "":
+        version = 2
+        track_string = f"/{track}/withusernames"
+    return f"http://frosthex.com:30050/api/v{version}/readonly/tracks{track_string}{API_KEY}"
 
 def get_track_info(track_name, count, max_count):
-  print(f"Got info from {track_name}! ({count}/{max_count})")
-  return requests.get(get_api_path(track_name)).json()
+    """Request and return the track info"""
+    print(f"{Fore.GREEN}[INFO]{Fore.WHITE} Got info from {Fore.LIGHTYELLOW_EX}{track_name}{Fore.WHITE}! ({count}/{max_count})")
+    return requests.get(get_api_path(track_name)).json()
 
 def calc_pp(self_ticks, sr_ticks):
-  return 100 * pow(0.985, (self_ticks - sr_ticks) / (sr_ticks / 2000))
+    """Calculate the pp based on record ticks and server record ticks"""
+    return 100 * pow(0.985, (self_ticks - sr_ticks) / (sr_ticks / 2000))
+
+def save_file(file_path, data):
+    """Save data to a file"""
+    with open(file_path, "w") as file:
+        file.write(data)
+        print(f"{Fore.CYAN}[DATA]{Fore.WHITE} Saved {Fore.LIGHTYELLOW_EX}{file_path}{Fore.WHITE}.")
 
 def gather_data():
-  tracks = requests.get(get_api_path()).json()
-  count = 1
-  track_count = tracks["number"]
-  for track in tracks["tracks"]:
-    track_info = get_track_info(track["command_name"], count, track_count)
-    sr = track_info["top_list"][0]
-    track_data[track["command_name"]] = {}
-    track_data[track["command_name"]]["command_name"] = track["command_name"]
-    track_data[track["command_name"]]["display_name"] = track["display_name"]
-    track_data[track["command_name"]]["date_created"] = track["date_created"]
-    track_data[track["command_name"]]["type"] = track["type"]
-    track_data[track["command_name"]]["open"] = track["open"]
-    track_data[track["command_name"]]["owner"] = track["owner"]
-    track_data[track["command_name"]]["tags"] = track["tags"]
-    track_data[track["command_name"]]["records"] = []
-    record_pos = 1
-    for user in track_info["top_list"]:
-      if user["username"] in user_data:
-        user_data[user["username"]]["pp"] += calc_pp(user["time"] / 20, sr["time"] / 20)
-        user_data[user["username"]]["records"].append({
-          "track_name": track["display_name"],
-          "position": record_pos,
-          "pp": calc_pp(user["time"] / 20, sr["time"] / 20)
-        })
-      else:
-        user_data[user["username"]] = {}
-        user_data[user["username"]]["username"] = user["username"]
-        user_data[user["username"]]["pp"] = calc_pp(user["time"] / 20, sr["time"] / 20)
-        user_data[user["username"]]["uuid"] = user["player_uuid"]
-        user_data[user["username"]]["records"] = []
-      track_data[track["command_name"]]["records"].append({
-        "username": user["username"],
-        "pp": calc_pp(user["time"] / 20, sr["time"] / 20),
-        "uuid": user["player_uuid"],
-        "time": user["time"]
-      })
-      record_pos += 1
-    count += 1
-    if count > 4:
-      return
+    """Gathering all the necessary data"""
+    tracks = requests.get(get_api_path()).json()
+    track_count = tracks["number"]
+    # Go through all tracks
+    for i, track in enumerate(tracks["tracks"], start=1):
+        track_info = get_track_info(track["command_name"], i, track_count)
+        if len(track_info["top_list"]) == 0:
+            print(f"{Fore.YELLOW}[WARNING]{Fore.LIGHTYELLOW_EX} No records found on {track['command_name']}!{Fore.WHITE}")
+            continue
+        sr = track_info["top_list"][0]
+
+        track_overview_data.update({track["command_name"]: {
+            "command_name": track["command_name"],
+            "display_name": track["display_name"],
+            "date_created": track["date_created"],
+            "record_count": len(track_info["top_list"])
+        }})
+
+        track_specific_data.update({track["command_name"]: {
+            "command_name": track["command_name"],
+            "display_name": track["display_name"],
+            "date_created": track["date_created"],
+            "type": track["type"],
+            "open": track["open"],
+            "owner": track["owner"],
+            "tags": track["tags"],
+            "records": []
+        }})
+
+        # Go through all records in the current track
+        for position, record in enumerate(track_info["top_list"], start=1):
+            if record["username"] in user_overview_data:
+                user_overview_data[record["username"]]["pp"] += calc_pp(record["time"] / 20, sr["time"] / 20)
+                user_specific_data[record["username"]]["pp"] = user_overview_data[record["username"]]["pp"]
+                user_specific_data[record["username"]]["records"].append({
+                    "track_name": track["display_name"],
+                    "position": position,
+                    "pp": calc_pp(record["time"] / 20, sr["time"] / 20)
+                })
+            else:
+              user_overview_data.update({record["username"]: {
+                  "username": record["username"],
+                  "pp": calc_pp(record["time"] / 20, sr["time"] / 20),
+                  "uuid": record["player_uuid"]
+              }})
+              user_specific_data.update({record["username"]: {
+                  "username": record["username"],
+                  "pp": calc_pp(record["time"] / 20, sr["time"] / 20),
+                  "uuid": record["player_uuid"],
+                  "records": [{
+                      "track_name": track["display_name"],
+                      "position": position,
+                      "pp": calc_pp(record["time"] / 20, sr["time"] / 20)
+                  }]
+              }})
+
+            track_specific_data[track["command_name"]]["records"].append({
+                "username": record["username"],
+                "pp": calc_pp(record["time"] / 20, sr["time"] / 20),
+                "uuid": record["player_uuid"],
+                "time": record["time"],
+                "position": position
+            })
+
+        save_file(f"tracks/{track['command_name']}.json", json.dumps(track_specific_data[track["command_name"]]))
+        #if i > 4:
+        #    return
 
 gather_data()
-with open("user_data.json", "w") as file:
-  file.write(json.dumps(user_data))
-  print("Saved user_data.json")
-with open("track_data.json", "w") as file:
-  file.write(json.dumps(track_data))
-  print("Saved track_data.json")
+
+for user in user_specific_data:
+    save_file(f"users/{user}.json", json.dumps(user_specific_data[user]))
+
+save_file("user_overview.json", json.dumps(user_overview_data))
+save_file("track_overview.json", json.dumps(track_overview_data))
